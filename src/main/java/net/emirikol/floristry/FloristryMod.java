e806325497c8e16ac9402e947491817144bfe44b
Emirikol <77687278+emirik0l@users.noreply.github.com>
2025-01-05 21:09:00 +0000

    add logic to scan for nearby flowers at the point of pollination

--- src/main/java/net/emirikol/floristry/FloristryMod.java
+++ src/main/java/net/emirikol/floristry/FloristryMod.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 
 public class FloristryMod implements ModInitializer {
 	public static final String MOD_ID = "floristry";
+	public static final int FLOWER_SCAN_RANGE = 5;
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
--- src/main/java/net/emirikol/floristry/mixin/PollinateGoalMixin.java
+++ src/main/java/net/emirikol/floristry/mixin/PollinateGoalMixin.java
@@ -2,6 +2,7 @@ package net.emirikol.floristry.mixin;
 
 import net.emirikol.floristry.FloristryMod;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.passive.BeeEntity;
 import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
@@ -9,6 +10,9 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Mixin(targets = "net.minecraft.entity.passive.BeeEntity$PollinateGoal")
 public abstract class PollinateGoalMixin {
@@ -22,10 +26,31 @@ public abstract class PollinateGoalMixin {
 
 	@Inject(method = "stop", at = @At("TAIL"), cancellable = true)
 	private void stopPollinating(CallbackInfo info) {
-		BlockPos flowerPos = beeEntity.getFlowerPos();
-		if (flowerPos != null && this.completedPollination()) {
-			Block block = beeEntity.getWorld().getBlockState(flowerPos).getBlock();
-			FloristryMod.LOGGER.info("A bee successfully got pollen from: " + block.getName().getString());
+		Block donor = this.getPollenDonor();
+		if (donor != null) {
+			FloristryMod.LOGGER.info("A bee successfully got pollen from: " + donor.getName().getString());
+			List<Block> nearbyFlowers = this.getNearbyFlowers();
+			for (Block nearbyFlower : nearbyFlowers) {
+				FloristryMod.LOGGER.info("\tNearby flower: " + nearbyFlower.getName().getString());
+			}
 		}
+	}
+
+	public Block getPollenDonor() {
+		BlockPos flowerPos = beeEntity.getFlowerPos();
+		if (flowerPos == null || !this.completedPollination()) { return null; }
+		return beeEntity.getWorld().getBlockState(flowerPos).getBlock();
+	}
+
+	public List<Block> getNearbyFlowers() {
+		List<Block> output = new ArrayList<Block>();
+		Iterable<BlockPos> iterable = BlockPos.iterateOutwards(beeEntity.getBlockPos(), FloristryMod.FLOWER_SCAN_RANGE, FloristryMod.FLOWER_SCAN_RANGE, FloristryMod.FLOWER_SCAN_RANGE);
+		for (BlockPos pos : iterable) {
+			BlockState blockState = beeEntity.getWorld().getBlockState(pos);
+			if (BeeEntity.isAttractive(blockState)) {
+				output.add(blockState.getBlock());
+			}
+		}
+		return output;
 	}
 }
