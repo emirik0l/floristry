4dc7d3cde055ba5a5e839c9130eb51d9f6dd1647
Emirikol <77687278+emirik0l@users.noreply.github.com>
2025-01-07 11:35:55 +0000

    add texture and model for new flower

---
+++ src/client/java/net/emirikol/floristry/FloristryClient.java
@@ -0,0 +1,13 @@
+package net.emirikol.floristry;
+
+import net.emirikol.floristry.block.FloristryBlocks;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
+
+public class FloristryClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		BlockRenderLayerMap.INSTANCE.putBlock(FloristryBlocks.WITHER_BUSH, RenderLayer.getCutout());
+	}
+}
