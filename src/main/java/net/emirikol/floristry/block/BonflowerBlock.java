6d1508f3c5feea265b70938156218feb2ecd3a09
Emirikol <77687278+emirik0l@users.noreply.github.com>
2025-01-12 22:29:59 +0000

    add smoke particles and light to bonflower

--- src/main/java/net/emirikol/floristry/block/BonflowerBlock.java
+++ src/main/java/net/emirikol/floristry/block/BonflowerBlock.java
@@ -1,13 +1,34 @@
 package net.emirikol.floristry.block;
 
 import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.FlowerBlock;
-import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.World;
 
 public class BonflowerBlock extends FlowerBlock {
 	public BonflowerBlock(AbstractBlock.Settings settings) {
-		super(StatusEffects.NIGHT_VISION, 5.0F, settings);
+		super(StatusEffects.NIGHT_VISION, 5.0F, settings.luminance((state) -> 14));
+	}
+
+	@Override
+	public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
+		VoxelShape voxelShape = this.getOutlineShape(state, world, pos, ShapeContext.absent());
+		Vec3d vec3d = voxelShape.getBoundingBox().getCenter();
+		double d = (double)pos.getX() + vec3d.x;
+		double e = (double)pos.getZ() + vec3d.z;
+
+		for(int i = 0; i < 3; ++i) {
+			if (random.nextBoolean()) {
+				world.addParticle(ParticleTypes.SMOKE, d + random.nextDouble() / (double)5.0F, (double)pos.getY() + ((double)0.5F - random.nextDouble()), e + random.nextDouble() / (double)5.0F, 0.0F, 0.0F, 0.0F);
+			}
+		}
+
 	}
 }
