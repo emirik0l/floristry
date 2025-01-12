2d0b313b50afe3999aacb7988cc12ee686f7683a
Emirikol <77687278+emirik0l@users.noreply.github.com>
2025-01-12 15:22:56 +0000

    add nectar component to beehive

--- src/main/java/net/emirikol/floristry/FloristryComponents.java
+++ src/main/java/net/emirikol/floristry/FloristryComponents.java
@@ -1,19 +1,27 @@
 package net.emirikol.floristry;
 
 import net.emirikol.floristry.component.NectarComponent;
+import net.minecraft.block.entity.BeehiveBlockEntity;
 import net.minecraft.entity.passive.BeeEntity;
 import net.minecraft.util.Identifier;
+import org.ladysnake.cca.api.v3.block.BlockComponentFactoryRegistry;
+import org.ladysnake.cca.api.v3.block.BlockComponentInitializer;
 import org.ladysnake.cca.api.v3.component.ComponentKey;
 import org.ladysnake.cca.api.v3.component.ComponentRegistry;
 import org.ladysnake.cca.api.v3.entity.EntityComponentFactoryRegistry;
 import org.ladysnake.cca.api.v3.entity.EntityComponentInitializer;
 
-public class FloristryComponents implements EntityComponentInitializer {
+public class FloristryComponents implements EntityComponentInitializer, BlockComponentInitializer {
 	public static final Identifier NECTAR_SOURCE_ID = Identifier.of(FloristryMod.MOD_ID, "nectar_source");
 	public static final ComponentKey<NectarComponent> NECTAR_SOURCE = ComponentRegistry.getOrCreate(NECTAR_SOURCE_ID, NectarComponent.class);
 
 	@Override
 	public void registerEntityComponentFactories(EntityComponentFactoryRegistry registry) {
 		registry.registerFor(BeeEntity.class, NECTAR_SOURCE, entity -> new NectarComponent());
+	}
+
+	@Override
+	public void registerBlockComponentFactories(BlockComponentFactoryRegistry registry) {
+		registry.registerFor(BeehiveBlockEntity.class, NECTAR_SOURCE, beehiveBlockEntity -> new NectarComponent());
 	}
 }
